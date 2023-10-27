87858e3c2b1502ac72618e6e3de8a50a87b1d8e1
shiweifu <shiweifu@gmail.com>
2023-10-27 20:49:35 +0800

    feat:调整文件结构

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+const disabledCss = {
+  "code::before": false,
+  "code::after": false,
+  "blockquote p:first-of-type::before": false,
+  "blockquote p:last-of-type::after": false,
+  pre: false,
+  code: false,
+  "pre code": false,
+};
+
 module.exports = {
   content: [
     "./templates/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {css: disabledCss},
+        sm: {css: disabledCss},
+        lg: {css: disabledCss},
+        xl: {css: disabledCss},
+        "2xl": {css: disabledCss},
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 }
